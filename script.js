df5ceea855a761097059eeecf34f89988c6c80ad
Anshz98 <140677743+Anshz98@users.noreply.github.com>
2024-10-07 20:00:02 -0600

    new proj

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const changeColorBtn = document.getElementById('changeColorBtn');
+    const body = document.body;
+
+    changeColorBtn.addEventListener('click', function() {
+        // Generate a random color
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        body.style.backgroundColor = "#" + randomColor;
+    });
+});
